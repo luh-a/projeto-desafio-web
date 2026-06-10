801f35a0861d181ec35162138033f2b9859b6dcd
Anna Luz <anna.mecenas@aluno.senai.br>
2026-06-09 21:44:15 -0300

    Criação da função menor/maior de idade

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 //Pegando elementos do dom
 const formPessoa = document.querySelector("#formulário-pessoa")
 const divDados = document.querySelector ("#div-dados")
+
+formPessoa.addEventListener('submit',(evt)=> {
+
+    evt.preventDefault()
+
+    const dadosForm = new FormData (formPessoa)
+
+    const nome = dadosForm.get('nome')
+    const idade = dadosForm.get('idade')
+
+    let situacaoIdade = idade >= 18 ? `${nome}, é Maior de idade` : `${nome}, é Menor de idade`
+    
+    divDados.innerHTML = situacaoIdade
+
+})
