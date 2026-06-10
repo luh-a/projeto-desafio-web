33f9467d4188525e91e90e91de9959af35f1da7f
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-10 20:05:33 -0300

    Criação do corpo de script_fundamentos.js

---
+++ js/script_fundamentos.js
@@ -0,0 +1,33 @@
+//comentário de uma linha
+/*
+    Comentário de múltiplas linhas
+    É possível ter várias
+    linhas
+*/
+console.log("Olá Mundo")
+
+//Declaração da variável
+let num = 10
+var num2 = 2
+const num3 = 100
+
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+
+if (true){
+    let num4 = 250
+    console.log(num4)
+
+    var num5 = 140
+}
+
+console.log(num5)
+
+num = 1882
+
+console.log(num)
+
+num2 = 58
